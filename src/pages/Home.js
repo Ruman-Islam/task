33c2d09609d1888324e47393acb1657e2fe8bc00
Ruman-Islam <rumanislam48@gmail.com>
2022-07-14 15:38:02 +0600

    test

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import OurCustomer from '../components/OurCustomer';
+import OurProduct from '../components/OurProduct';
+import Pricing from '../components/Pricing';
+import WhatWeDo from '../components/WhatWeDo';
+
+const Home = () => {
+    return (
+        <div className='bg-light px-4'>
+            <Hero />
+            <OurCustomer />
+            <WhatWeDo />
+            <OurProduct />
+            <Pricing />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    );
+};
+
+export default Home;
